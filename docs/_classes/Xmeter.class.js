e1e0ec01d2db8f7d2235089d8372493440816520
Chris Harvey <chrisharvey2pi@gmail.com>
2016-08-14 12:30:42 -0400

    Xmeter.DOCS instanceof Styleguide

--- docs/_classes/Xmeter.class.js
+++ docs/_classes/Xmeter.class.js
@@ -1,37 +1,38 @@
+// var Styleguide = require('../_classes/Styleguide.class.js')
+
 var Xmeter = (function () {
   // CONSTRUCTOR
   function Xmeter() {
-    var self = this
-    Styleguide.call(self, 'Xmeter', '/docs/' )
-    Styleguide.prototype.setTitle.call(self, 'Xmeter Style Guide')
   }
-  Xmeter.prototype = Object.create(Styleguide.prototype)
-  Xmeter.prototype.constructor = Xmeter
 
   // METHODS
-  Xmeter.prototype.init = function init() {
-    var self = this
-    Styleguide.prototype.init.call(self)
-    self.getPage('visual.html').hide()
-    self.getPage('obj.html')
+
+  // STATIC MEMBERS
+  Xmeter.DOCS = new Styleguide('Xmeter', '/docs/')
+    .setTitle('Xmeter Style Guide')
+    .init()
+  ;(function () {
+    // adding pages to Xmeter.DOCS
+    Xmeter.DOCS.getPage('visual.html')
+      .hide()
+    Xmeter.DOCS.getPage('obj.html')
       .addPage(new Page({ name: 'Table of Contents', url: '#table-contents' }))
       .addPage(new Page({ name: 'The List Object'  , url: '#list-object' }))
       .addPage(new Page({ name: 'The Flex Object'  , url: '#flex-object' }))
       .addPage(new Page({ name: 'The Grid Object'  , url: '#grid-object' }))
-    self.getPage('comp.html').hide()
-    self.getPage('help.html')
+    Xmeter.DOCS.getPage('comp.html')
+      .hide()
+    Xmeter.DOCS.getPage('help.html')
       .addPage(new Page({ name: 'Table of Contents', url: '#table-contents' }))
       .addPage(new Page({ name: 'Block'            , url: '#block' }))
       .addPage(new Page({ name: 'Inline'           , url: '#inline' }))
       .addPage(new Page({ name: 'Clearfix'         , url: '#cleafix' }))
-    self.getPage('atom.html')
+    Xmeter.DOCS.getPage('atom.html')
       .addPage(new Page({ name: 'Table of Contents', url: '#table-contents' }))
       .addPage(new Page({ name: 'margin-bottom'    , url: '#margin-bottom' }))
       .addPage(new Page({ name: 'padding-top'      , url: '#padding-top' }))
-    return self
-  }
+  })()
 
-  // STATIC MEMBERS
   Xmeter.DOCS_CLASSES = {
     figure : 'docs-figure'
   , pre    : 'docs-pre'
