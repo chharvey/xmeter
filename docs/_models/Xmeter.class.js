759709b2a1e64434633f128d4b9b8893b79e3522
Chris Harvey <chrisharvey2pi@gmail.com>
2017-03-10 21:15:24 -0500

    docs absolute urls

--- docs/_models/Xmeter.class.js
+++ docs/_models/Xmeter.class.js
@@ -1,12 +1,12 @@
 var Page = require('sitepage').Page
 
-/**
- * A set of static members used for the site.
- * Similar to a utility class.
- * @type {Xmeter}
- */
 module.exports = (function () {
   // CONSTRUCTOR
+  /**
+   * A set of static members used for the site.
+   * Similar to a utility class.
+   * @constructor
+   */
   function Xmeter() {}
 
   // METHODS
@@ -17,47 +17,47 @@ module.exports = (function () {
    * @type {Page}
    */
   Xmeter.DOCS = new Page({ name: 'Xmeter Style Guide', url: '/docs/' })
-      .title('Xmeter Style Guide')
-      .description('A demo of Xmeter styles.')
-      .add(new Page({ name: 'Home', url: 'index.html' })
-        .description('Xmeter Homepage')
+    .title('Xmeter Style Guide')
+    .description('A demo of Xmeter styles.')
+    .add(new Page({ name: 'Home', url: '/docs/index.html' })
+      .description('Xmeter Homepage')
+    )
+    .add(new Page({ name: 'Base Typography', url: '/docs/base.html' })
+      .description('Bare, unstyled HTML elements. No classes.')
+      .add(new Page({ name: 'Grouping Elements', url: '/docs/base.html#grouping-elements' })
+        .add(new Page({ name: 'Headings & Paragraphs', url: '/docs/base.html#headings-paragraphs' }))
+        .add(new Page({ name: 'Lists'                , url: '/docs/base.html#lists' }))
+        .add(new Page({ name: 'Tables'               , url: '/docs/base.html#tables' }))
       )
-      .add(new Page({ name: 'Base Typography', url: 'base.html' })
-        .description('Bare, unstyled HTML elements. No classes.')
-        .add(new Page({ name: 'Grouping Elements', url: 'base.html#grouping-elements' })
-          .add(new Page({ name: 'Headings & Paragraphs', url: 'base.html#headings-paragraphs' }))
-          .add(new Page({ name: 'Lists'                , url: 'base.html#lists' }))
-          .add(new Page({ name: 'Tables'               , url: 'base.html#tables' }))
-        )
-        .add(new Page({ name: 'Text-Level Elements', url: 'base.html#text-level-elements' })
-          .add(new Page({ name: 'Links'        , url: 'base.html#links' }))
-          .add(new Page({ name: 'Stress'       , url: 'base.html#stress' }))
-          .add(new Page({ name: 'Documentation', url: 'base.html#documentation' }))
-          .add(new Page({ name: 'Data'         , url: 'base.html#data' }))
-        )
-        .add(new Page({ name: 'Forms'               , url: 'base.html#forms' }))
-        .add(new Page({ name: 'Embedded Elements'   , url: 'base.html#embedded-elements' }))
-        .add(new Page({ name: 'Interactive Elements', url: 'base.html#interactive-elements' }))
+      .add(new Page({ name: 'Text-Level Elements', url: '/docs/base.html#text-level-elements' })
+        .add(new Page({ name: 'Links'        , url: '/docs/base.html#links' }))
+        .add(new Page({ name: 'Stress'       , url: '/docs/base.html#stress' }))
+        .add(new Page({ name: 'Documentation', url: '/docs/base.html#documentation' }))
+        .add(new Page({ name: 'Data'         , url: '/docs/base.html#data' }))
       )
-      .add(new Page({ name: 'Objects', url: 'obj.html' })
-        .description('Patterns of structure that can be reused many times for many different purposes.')
-        .add(new Page({ name: 'The List Object'  , url: 'obj.html#list-object' }))
-        .add(new Page({ name: 'The Flex Object'  , url: 'obj.html#flex-object' }))
-        .add(new Page({ name: 'The Grid Object'  , url: 'obj.html#grid-object' }))
-      )
-      .add(new Page({ name: 'Helpers', url: 'help.html' })
-        .description('Somewhat explicit classes used for enhancing default styles.')
-        .add(new Page({ name: 'Block'            , url: 'help.html#block' }))
-        .add(new Page({ name: 'Inline'           , url: 'help.html#inline' }))
-        .add(new Page({ name: 'Clearfix'         , url: 'help.html#clearfix' }))
-        .add(new Page({ name: 'Hidden'           , url: 'help.html#hidden' }))
-      )
-      .add(new Page({ name: 'Atoms', url: 'atom.html' })
-        .description('Very specific classes used for creating anomalies or fixing broken styles.')
-        .add(new Page({ name: 'margin-bottom'    , url: 'atom.html#margin-bottom' }))
-        .add(new Page({ name: 'padding-top'      , url: 'atom.html#padding-top' }))
-        .add(new Page({ name: 'font-size'        , url: 'atom.html#font-size' }))
-      )
+      .add(new Page({ name: 'Forms'               , url: '/docs/base.html#forms' }))
+      .add(new Page({ name: 'Embedded Elements'   , url: '/docs/base.html#embedded-elements' }))
+      .add(new Page({ name: 'Interactive Elements', url: '/docs/base.html#interactive-elements' }))
+    )
+    .add(new Page({ name: 'Objects', url: '/docs/obj.html' })
+      .description('Patterns of structure that can be reused many times for many different purposes.')
+      .add(new Page({ name: 'The List Object'  , url: '/docs/obj.html#list-object' }))
+      .add(new Page({ name: 'The Flex Object'  , url: '/docs/obj.html#flex-object' }))
+      .add(new Page({ name: 'The Grid Object'  , url: '/docs/obj.html#grid-object' }))
+    )
+    .add(new Page({ name: 'Helpers', url: '/docs/help.html' })
+      .description('Somewhat explicit classes used for enhancing default styles.')
+      .add(new Page({ name: 'Block'            , url: '/docs/help.html#block' }))
+      .add(new Page({ name: 'Inline'           , url: '/docs/help.html#inline' }))
+      .add(new Page({ name: 'Clearfix'         , url: '/docs/help.html#clearfix' }))
+      .add(new Page({ name: 'Hidden'           , url: '/docs/help.html#hidden' }))
+    )
+    .add(new Page({ name: 'Atoms', url: '/docs/atom.html' })
+      .description('Very specific classes used for creating anomalies or fixing broken styles.')
+      .add(new Page({ name: 'margin-bottom'    , url: '/docs/atom.html#margin-bottom' }))
+      .add(new Page({ name: 'padding-top'      , url: '/docs/atom.html#padding-top' }))
+      .add(new Page({ name: 'font-size'        , url: '/docs/atom.html#font-size' }))
+    )
 
   return Xmeter
 })()
