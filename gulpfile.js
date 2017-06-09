f76b67322b15a86d567d15815f2430bb4a66ad7f
Chris Harvey <chrisharvey2pi@gmail.com>
2017-06-09 11:07:30 -0400

    add gulp task minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp')
+var rename = require('gulp-rename')
 var pug = require('gulp-pug')
 var less = require('gulp-less')
+var clean_css = require('gulp-clean-css')
 
 gulp.task('pug:docs', function () {
   return gulp.src('docs/{index,base,obj,comp,help,atom}.pug')
@@ -22,5 +24,12 @@ gulp.task('lessc:docs', function () {
 gulp.task('lessc:xmeter', function () {
   return gulp.src('xmeter.less')
     .pipe(less())
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('minify', ['lessc:xmeter'], function () {
+  return gulp.src('xmeter.css')
+    .pipe(clean_css())
+    .pipe(rename('xmeter.min.css')) // TODO: use a SourceMap!
     .pipe(gulp.dest('./'))
 })
