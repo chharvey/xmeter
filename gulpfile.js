97a3cb92d03305ac025fc1f1dd6063b6b2431933
Chris Harvey <chrisharvey2pi@gmail.com>
2018-02-22 20:17:06 -0500

    add gulp task lessc:each

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+
 const kss          = require('kss')
 const gulp         = require('gulp')
 const jsdoc        = require('gulp-jsdoc3')
@@ -38,6 +42,67 @@ gulp.task('lessc:docs', function () {
 })
 
 gulp.task('build:docs', ['docs:api', 'docs:kss', 'pug:docs', 'lessc:docs'])
+
+gulp.task('lessc:each', async function () {
+  let breakpoints = [
+    { suffix: '-sK', query: 'screen  and (min-width: 30em)' },
+    { suffix: '-sM', query: 'screen  and (min-width: 45em)' },
+    { suffix: '-sG', query: 'screen  and (min-width: 60em)' },
+    { suffix: '-sT', query: 'screen  and (min-width: 75em)' },
+    { suffix: '-sP', query: 'screen  and (min-width: 90em)' },
+    { suffix: '-nK', query: 'not all and (min-width: 30em)' },
+    { suffix: '-nM', query: 'not all and (min-width: 45em)' },
+    { suffix: '-nG', query: 'not all and (min-width: 60em)' },
+    { suffix: '-nT', query: 'not all and (min-width: 75em)' },
+    { suffix: '-nP', query: 'not all and (min-width: 90em)' },
+  ]
+
+  let files = [
+    // TODO use `fs.readdir(path.resolve(__dirname, './css/src/'))` and filter out only the ones you want
+    'o-List',
+    'o-Flex',
+    'o-Grid',
+    'h-Block',
+    'h-Inline',
+    'h-Clearfix',
+    'h-Measure',
+    'h-Ruled',
+  ]
+
+  let classes = [
+    'o-List',
+    'o-List__Item',
+    'o-Flex',
+    'o-Flex__Item',
+    'o-Grid',
+    'o-Grid__Item',
+    'h-Block',
+    'h-Inline',
+    'h-Clearfix',
+    'h-Measure',
+    'h-Measure--narrow',
+    'h-Measure--wide',
+    'h-Ruled',
+  ]
+
+  let stylesheets = breakpoints.map((bp) => ({
+    filename: `xmeter${bp.suffix}.less`,
+    contents: `
+      ${files.map((filename) => `@import (reference) url('../src/_${filename}');`).join('\n')}
+      @media ${bp.query} {
+        ${classes.map((classname) => `.${classname}${bp.suffix} { .${classname}; }`).join('\n')}
+      }
+    `,
+  }))
+
+  try {
+    await util.promisify(fs.mkdir)(path.resolve(__dirname, './css/build/'))
+  } catch (err) {}
+
+  await Promise.all(stylesheets.map((ss) =>
+    util.promisify(fs.writeFile)(path.resolve(__dirname, './css/build/', ss.filename), ss.contents, 'utf8')
+  ))
+})
 
 gulp.task('lessc:core', function () {
   return gulp.src('css/src/xmeter.less')
