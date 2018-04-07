59da43524e26c921da74f823c48dda9671b29a49
Chris Harvey <chrisharvey2pi@gmail.com>
2018-04-07 01:44:11 -0400

    make gulp task render:docs

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const path = require('path')
 const util = require('util')
 
 const kss          = require('kss')
+const jsdom        = require('jsdom')
 const gulp         = require('gulp')
 const jsdoc        = require('gulp-jsdoc3')
 const pug          = require('gulp-pug')
@@ -10,6 +11,8 @@ const less         = require('gulp-less')
 const autoprefixer = require('gulp-autoprefixer')
 const clean_css    = require('gulp-clean-css')
 const sourcemaps   = require('gulp-sourcemaps')
+
+const {xDirectory} = require('aria-patterns')
 
 const createDir = require('./lib/createDir.js')
 
@@ -21,6 +24,42 @@ gulp.task('docs:api', function () {
 
 gulp.task('docs:kss', function () {
   return kss(require('./config/kss.json'))
+})
+
+gulp.task('render:docs', async function () {
+  const dom = new jsdom.JSDOM(await util.promisify(fs.readFile)(path.join(__dirname, './docs/tpl/index.tpl.html'), 'utf8'))
+  const {document} = dom.window
+
+  async function importLinks(relativepath) {
+    if (!('import' in jsdom.JSDOM.fragment('<link rel="import" href="https://example.com/"/>').querySelector('link'))) {
+      console.warn('`HTMLLinkElement#import` is not yet supported. Replacing `<link>`s with their imported contents.')
+      return Promise.all(Array.from(document.querySelectorAll('link[rel~="import"][data-import]')).map(async function (link) {
+        const import_switch = {
+          'document': async () => jsdom.JSDOM.fragment(await util.promisify(fs.readFile)(path.resolve(relativepath, link.href), 'utf8')),
+          'template': async () => (await xjs.HTMLTemplateElement.fromFile(path.resolve(relativepath, link.href))).content(),
+          async default() { return null },
+        }
+        let imported = await (import_switch[link.getAttribute('data-import')] || import_switch.default).call(null)
+        if (imported) {
+          link.after(imported)
+          link.remove() // link.href = path.resolve('https://example.com/index.html', link.href) // TODO set the href relative to the current window.location.href
+        }
+      }))
+    } else return;
+  }
+
+  await importLinks(path.resolve(__dirname, './docs/tpl/'));
+
+  await Promise.all([
+    (async function () {
+      document.querySelector('#contents').append(xDirectory.render({}))
+    })(),
+    (async function () {
+      document.querySelector('main').append()
+    })(),
+  ])
+
+  await util.promisify(fs.writeFile)(path.resolve(__dirname, './docs/index.html'), dom.serialize(), 'utf8')
 })
 
 gulp.task('pug:docs', function () {
