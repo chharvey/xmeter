86c9f581ca3bd201405aa13f1ad53c061948da8f
Chris Harvey <chrisharvey2pi@gmail.com>
2018-11-22 00:51:48 -0500

    DELETE custom documentation

--- gulpfile.js
+++ gulpfile.js
@@ -65,55 +65,6 @@ gulp.task('docs-kss-style', async function () {
 
 gulp.task('docs-kss', ['docs-kss-markup', 'docs-kss-style'])
 
-gulp.task('docs-my-markup', async function () {
-  const dom = new jsdom.JSDOM(await util.promisify(fs.readFile)(path.join(__dirname, './docs/tpl/index.tpl.html'), 'utf8'))
-  const {document} = dom.window
-
-  await new xjs.Document(document).importLinksAsync(path.resolve(__dirname, './docs/tpl/'))
-
-  await Promise.all([
-    (async function () {
-      document.querySelector('#contents').append(xDirectory.render(require('./docs/index-toc.json')))
-    })(),
-    (async function () {
-      const classname = {
-        figure: 'docs-figure',
-        pre   : 'docs-pre',
-        code  : 'docs-code',
-        form  : 'docs-form',
-      }
-      let fragment = (await xjs.HTMLTemplateElement.fromFile(path.resolve(__dirname, './docs/tpl/base.tpl.html'))).content().cloneNode(true)
-      fragment.querySelectorAll([
-        'section[id] > h2:first-of-type',
-        'section[id] > h3:first-of-type',
-        'section[id] > h4:first-of-type',
-        'section[id] > h5:first-of-type',
-        'section[id] > h6:first-of-type',
-      ].join()).forEach((hn) => {
-        hn.append(xPermalink.render({ id: hn.parentNode.id }))
-      })
-      fragment.querySelectorAll(Object.keys(classname).join()).forEach((el) => {
-        let xel = new xjs.HTMLElement(el)
-        if (classname[xel.tagName]) xel.addClass(classname[xel.tagName])
-      })
-      document.querySelector('main').append(fragment)
-    })(),
-  ])
-
-  await util.promisify(fs.writeFile)(path.resolve(__dirname, './docs/index.html'), dom.serialize(), 'utf8')
-})
-
-gulp.task('docs-my-style', async function () {
-  return gulp.src('docs/css/docs.less')
-    .pipe(less())
-    .pipe(autoprefixer({
-      grid: true,
-    }))
-    .pipe(gulp.dest('./docs/css/'))
-})
-
-gulp.task('docs-my', ['docs-my-markup', 'docs-my-style'])
-
-gulp.task('docs', ['docs-kss', 'docs-my'])
+gulp.task('docs', ['docs-kss'])
 
 gulp.task('build', ['dist', 'docs'])
