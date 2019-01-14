abc948d63c94ab808f67c455caeccad3931b8a84
Chris Harvey <chrisharvey2pi@gmail.com>
2019-01-14 13:18:12 -0500

    clean dialog script

--- docs/js/base.js
+++ docs/js/base.js
@@ -1,9 +1,9 @@
-let interactive = document.querySelector('#kssref-base-interactive-elements')
-let dialog = interactive.querySelector('dialog')
-interactive.querySelector('button[name="dialog-open"]').addEventListener('click', function (e) {
-  dialog.showModal()
+var interactive = document.querySelector('#kssref-base-interactive-elements')
+var dialog = interactive.querySelector('dialog')
+interactive.querySelector('button[name="dialog-open"]').addEventListener('click', function (_e) {
+	dialog.showModal()
 })
-dialog.querySelector('button[name="dialog-close"]').addEventListener('click', function (e) {
-  dialog.close('some result')
-  // access result with `dialog.returnValue`
+dialog.querySelector('button[name="dialog-close"]').addEventListener('click', function (_e) {
+	dialog.close('some result')
+	// access result with `dialog.returnValue`
 })
